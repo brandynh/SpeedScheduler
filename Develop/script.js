e06d41992c061187bc7854197d37d5fefed08cac
Brandyn <hufferbrandyn@gmail.com>
2021-09-11 17:58:27 -0500

    removed unused DOM traversal consts added local set and get for schedule functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,37 @@
-const $containerEl = $('container');
-const $rowEl = $('row');
-const $hourEl = $('hour');
-const $timeBlockEl = $('time-block');
-const $saveButtonEl = $('saveBtn');
+$(document).ready(function () {
+    console.log("Document Ready!");
 
-var $currentDayEl = $("#currentDay");
-
-
-
-setInterval(displayTime, 1000);
-
-
-function displayTime() {
-
+    var $currentDayEl = $("#currentDay");
     var currentTime = moment().format('dddd, MMMM Do [at] hh:mm a');
-    $currentDayEl.text(currentTime);
 
-}
 
+
+    setInterval(displayTime, 1000);
+
+    function displayTime() {
+
+        $currentDayEl.text(currentTime);
+
+    }
+
+    $(".saveBtn").on("click", function(){
+        var value = $(this).siblings(".time-block").val();
+        var time = $(this).parent().attr("id");
+    
+        localStorage.setItem(time, value);
+    });
+
+    function hourUpdate() {
+
+    }
+
+    $("#9 .time-block").val(localStorage.getItem("9"));
+    $("#10 .time-block").val(localStorage.getItem("10"));
+    $("#11 .time-block").val(localStorage.getItem("11"));
+    $("#12 .time-block").val(localStorage.getItem("12"));
+    $("#1 .time-block").val(localStorage.getItem("1"));
+    $("#2 .time-block").val(localStorage.getItem("2"));
+    $("#3 .time-block").val(localStorage.getItem("3"));
+    $("#4 .time-block").val(localStorage.getItem("4"));
+    $("#5 .time-block").val(localStorage.getItem("5"));
+});
