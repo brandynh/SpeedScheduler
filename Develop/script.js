5b627623e67b26ad5b0fa4260042d098b37af50a
Brandyn <hufferbrandyn@gmail.com>
2021-09-12 16:28:23 -0500

    include interval to update row colors, loop(not working currently) used to interate through rows to add and remove css classes

--- Develop/script.js
+++ Develop/script.js
@@ -1,37 +1,71 @@
+// Ready document before initiating JS
 $(document).ready(function () {
-    console.log("Document Ready!");
+    //Display ready
+    console.log('Ready to execute!');
 
-    var $currentDayEl = $("#currentDay");
+    var $currentDayEl = $('#currentDay');
+
     var currentTime = moment().format('dddd, MMMM Do [at] hh:mm a');
+    var currentHour = parseInt(moment().format('HH'));
+    var rowTime = (parseInt($(".row").attr("id")));
 
 
+    // Sets interval to display formatted time
+    setInterval(displayContent, 1000);
+    // Sets interval and updates past, present, future
+    setInterval(updateSchedule, 10000);
 
-    setInterval(displayTime, 1000);
-
-    function displayTime() {
+    function displayContent() {
 
         $currentDayEl.text(currentTime);
 
+
     }
 
-    $(".saveBtn").on("click", function(){
+    function updateSchedule() {
+
+
+        console.log(currentHour);
+
+
+
+
+        $(".row").each(function () {
+
+            if (rowTime < currentHour) {
+
+                $(this).children("textarea").addClass("past")
+                console.log("past")
+            } else
+                if (rowTime === currentHour) {
+
+                    $(this).children("textarea").remove("past").addClass("present")
+
+                } else {
+                    $(this).children("textarea").removeClass("past", "present").addClass("future");
+
+                }
+            console.log(rowTime);
+        })
+
+    }
+
+    $(".saveBtn").on("click", function () {
+        // Sets variables to get values from text and times
         var value = $(this).siblings(".time-block").val();
         var time = $(this).parent().attr("id");
-    
+
         localStorage.setItem(time, value);
     });
 
-    function hourUpdate() {
-
-    }
 
     $("#9 .time-block").val(localStorage.getItem("9"));
     $("#10 .time-block").val(localStorage.getItem("10"));
     $("#11 .time-block").val(localStorage.getItem("11"));
     $("#12 .time-block").val(localStorage.getItem("12"));
-    $("#1 .time-block").val(localStorage.getItem("1"));
-    $("#2 .time-block").val(localStorage.getItem("2"));
-    $("#3 .time-block").val(localStorage.getItem("3"));
-    $("#4 .time-block").val(localStorage.getItem("4"));
-    $("#5 .time-block").val(localStorage.getItem("5"));
+    $("#13 .time-block").val(localStorage.getItem("13"));
+    $("#14 .time-block").val(localStorage.getItem("14"));
+    $("#15 .time-block").val(localStorage.getItem("15"));
+    $("#16 .time-block").val(localStorage.getItem("16"));
+    $("#17 .time-block").val(localStorage.getItem("17"));
 });
